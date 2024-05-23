7786d25f78bdc5edb3373d3808fc87ae4e96a4e2
Donguk Lee <115720827+HS-Donguk@users.noreply.github.com>
2024-05-23 16:17:12 +0900

    Update Local_Sensor_System1.ino

--- ArduinoSystem/System1/Local_System1/Local_Sensor_System1/Local_Sensor_System1.ino
+++ ArduinoSystem/System1/Local_System1/Local_Sensor_System1/Local_Sensor_System1.ino
@@ -145,29 +145,11 @@ void GPS(){
     float flat, flon;
     unsigned long age;
     gps.f_get_position(&flat, &flon, &age);
-    // Serial.print("LAT=");
-    // Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
     exLAT = (flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
     exLON = (flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
-    // Serial.print(ex_LAT);  // 임시
-    // Serial.print(" LON=");
-    // Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
-    // Serial.print(ex_LON);  // 임시
-    // Serial.print(" SAT=");
-    // Serial.print(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
-    // Serial.print(" PREC=");
-    // Serial.print(gps.hdop() == TinyGPS::GPS_INVALID_HDOP ? 0 : gps.hdop());
   }
   
   gps.stats(&chars, &sentences, &failed);
-  // Serial.print(" CHARS=");
-  // Serial.print(chars);
-  // Serial.print(" SENTENCES=");
-  // Serial.print(sentences);
-  // Serial.print(" CSUM ERR=");
-  // Serial.println(failed);
-  // if (chars == 0)
-  //   Serial.println("** No characters received from GPS: check wiring **");
 
 }
 
